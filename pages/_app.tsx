03436dcb7958c83d98b677a2a046f4246e03220b
Hoang Nguyen <hdnguyen0305@gmail.com>
2022-07-20 17:34:56 +0700

    react query data fetching

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,19 @@ import '../styles/global.scss'
 import { EmptyLayout } from '@/components/layout'
 import { Provider } from 'react-redux'
 import { store } from '@/app/store'
-import { injectStore } from '@/api/axios-client';
-injectStore(store);
+import { injectStore } from '@/api/axios-client'
+injectStore(store)
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { ReactElement, ReactNode } from 'react'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
 
 export interface LayoutProps {
-  children: ReactNode,
+  children: ReactNode
   className?: string
 }
 
@@ -21,15 +26,18 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout
 
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
     </Provider>
   )
 }
--- pages/homepage/index.tsx
+++ pages/homepage/index.tsx
@@ -1,13 +1,38 @@
 import { MainLayout } from '@/components/layout'
 import Head from 'next/head'
 import React from 'react'
+import { useMutation } from '@tanstack/react-query'
+import axios, { AxiosError } from 'axios'
+import { Button } from '@/components/styled'
+import { LoginResponse } from '@/models/api'
+
+interface LoginSubmit {
+  username: string
+  password: string
+}
 
 const HomePage = () => {
+  const mutation = useMutation<LoginResponse,AxiosError,LoginSubmit,LoginResponse>((data) => axios.post('http://180.93.175.236:3000/staff/login', data))
+
   return (
     <>
       <Head>
         <title>Home Page</title>
       </Head>
+
+      {mutation.isError && (
+        <div>An error occurred: {mutation.error.message}</div>
+      )}
+
+      {mutation.isSuccess && <p>{mutation.data.toString()}</p>}
+
+      <Button
+        onClick={() => mutation.mutate({ username: 'dungnh', password: '2' })}
+        posting={mutation.isLoading}
+      >
+        Submit
+      </Button>
+
       <div>HomefPffage</div>
     </>
   )
